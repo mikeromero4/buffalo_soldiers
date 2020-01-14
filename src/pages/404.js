2c9da7dec17c8bcf3f2471a91ffc89445f2913c3
mikeromero4 <mikeromero4@yahoo.com>
2020-01-13 22:34:38 -0800

    test

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,6 +3,7 @@ import React from "react"
 import Layout from "../components/templates/generic/layout"
 import SEO from "../components/utilities/seo"
 
+//test
 const NotFoundPage = () => (<>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
